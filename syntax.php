e7ca03e7d09394e71575d850b1f687f06bad1a13
sumonkhan60 <sumonkhan60@yahoo.com>
2019-12-03 20:30:20 +0600

    syntax.php file added

---
+++ syntax.php
@@ -0,0 +1,45 @@
+<?php
+//    echo "<html>";
+//        echo "<head>";
+//            echo "<title>";
+//                echo "HOME";
+//            echo "</title>";
+//        echo "</head>";
+//        echo "<body>";
+//            echo "<h1>This is a heading.</h1>";
+//            echo "<p>This is a paragraph.</p>";
+//        echo "</body>";
+//    echo "</html>";
+?>
+
+<!--<html>-->
+<!--    <head>-->
+<!--        <title>--><?php //echo "HOME"; ?><!--</title>-->
+<!--    </head>-->
+<!--    <body>-->
+<!--        <h1>--><?php //echo  "This is a heading. "?><!--</h1>-->
+<!--        <p>--><?php //echo  "This is a paragraph. "?><!--</p>-->
+<!--        <input type="text" value="--><?php //echo "Hello World"; ?><!--" />-->
+<!--    </body>-->
+<!--</html>-->
+
+<?php
+
+//    $firstName = "Sumon";
+//    $lastName = "Khan";
+//    echo $firstName.' '.$lastName;
+//    $name = 123.123;
+
+      $x = 10;
+      $y = 20;
+
+      echo $x++; //10
+      echo '<br>';
+      echo --$x; //10
+      echo '<br>';
+      echo $x--; //10
+      echo '<br>';
+      echo --$x; //8
+
+
+?>
