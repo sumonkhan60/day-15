bebaf4a63517c4fcd393f1dbb863e0aa142c3295
sumonkhan60 <sumonkhan60@yahoo.com>
2019-12-03 20:53:01 +0600

    practice.php file added

---
+++ practice.php
@@ -0,0 +1,46 @@
+<?php
+
+$firstNumber = 10;
+$secondNumber = 30;
+
+echo (--$firstNumber)."<br/>";
+echo ($firstNumber--)."<br/>";
+echo ($firstNumber++)."<br/>";
+echo ($firstNumber--)."<br/>";
+
+
+
+
+
+
+
+//do{
+//    echo $firstNumber."<br/>";
+//    $firstNumber++;
+//}while($firstNumber <= 20);
+
+//while ($firstNumber <= 20)
+//{
+//    echo $firstNumber."<br/>";
+//    $firstNumber++;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+//$firstNumber = 10;
+//$secondNumber = 10;
+//if($firstNumber == $secondNumber){
+//    echo "Hello World";
+//}else{
+//    echo "Hello BITM";
+//}
+
